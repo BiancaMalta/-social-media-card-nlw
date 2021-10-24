655b8eedca7fd480abf6c25e4d139bbd267edc47
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2021-10-23 21:22:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+    github: 'BiancaMalta',
+    youtube: 'channel/UC8ozobZ5QdLUq61Ot_LTvng',
+    facebook: 'bianca.malta.395',
+    instagram: 'atlam._/',
+    twitter: 'BiancaAtlam'
+}
+
+function changeSocialMediaLinks() {
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class')
+
+        li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+    }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            userName.textContent = data.name
+            userBio.textContent = data.bio
+            userLink.href = data.html_url
+            UserImage.src = data.avatar_url
+            userLogin.textContent = data.login
+        })
+}
+getGitHubProfileInfos()
